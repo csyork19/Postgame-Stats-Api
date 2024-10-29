08e386cf19f24ac7b59228686b16b18b1396acea
csyork19 <csyork19@gmail.com>
2024-10-29 16:14:16 -0400

    Setup player and team averager rest api endpoints

--- Postgame Stats/NbaPlayerStats.py
+++ Postgame Stats/NbaPlayerStats.py
@@ -4,11 +4,25 @@ import PostGameStatsUtil
 
 def get_player_stats(self):
     if self:
-        player_logs = playergamelog.PlayerGameLog(
-            player_id=PostGameStatsUtil.PostGameStatsUtil.get_player_id(self))
+        player_logs = playergamelog.PlayerGameLog(player_id=PostGameStatsUtil.PostGameStatsUtil.get_player_id(self))
         player_logs_df = player_logs.get_data_frames()[0]
         return player_logs_df.to_dict()
     return None
+
+def get_player_season_average(self):
+    player_logs = playergamelog.PlayerGameLog(player_id=PostGameStatsUtil.PostGameStatsUtil.get_player_id(self))
+    player_logs_df = player_logs.get_data_frames()[0]
+
+    # Define the columns you want to average
+    columns_to_average = [
+        "AST", "BLK", "DREB", "FG3A", "FG3M", "FG3_PCT", "FGA", "FGM", "FG_PCT",
+        "FTA", "FTM", "FT_PCT", "MIN", "OREB", "PF", "PLUS_MINUS", "PTS", "REB", "STL", "TOV"
+    ]
+
+    # Filter the DataFrame to only include these columns
+    filtered_df = player_logs_df[columns_to_average]
+    averages = filtered_df.mean()
+    return averages.to_dict()
 
 
 def get_player_career_stats(self):
--- Postgame Stats/app.py
+++ Postgame Stats/app.py
@@ -1,11 +1,6 @@
 from flask import Flask, request
-from nba_api.stats.endpoints import PlayerGameLogs
-
 import NbaPlayerStats
 import TeamStats
-import WnbaPlayerStats
-import GleaguePlayerStats
-import NcaaBasketballStats
 from flask_cors import CORS
 
 app = Flask(__name__)
@@ -19,6 +14,13 @@ def player_season_stats():
         return NbaPlayerStats.get_player_stats(player_name)
 
 
+@app.route('/api/nba/player/seasonAverages', methods=['POST'])
+def player_season_average_stats():
+    if request.is_json:
+        player_name = request.get_json()['playerName']
+        return NbaPlayerStats.get_player_season_average(player_name)
+
+
 @app.route('/api/nba/player/careerStats', methods=['POST'])
 def player_career_stats():
     if request.is_json:
@@ -28,6 +30,13 @@ def player_career_stats():
 
 @app.route('/api/nba/player/playoffStats', methods=['POST'])
 def player_playoff_stats():
+    if request.is_json:
+        player_name = request.get_json()['playerName']
+        season = request.get_json()['season']
+        return NbaPlayerStats.get_player_playoff_stats(player_name, season)
+
+@app.route('/api/nba/player/playoffStatsAverage', methods=['POST'])
+def player_playoff_average_stats():
     if request.is_json:
         player_name = request.get_json()['playerName']
         season = request.get_json()['season']
@@ -52,6 +61,16 @@ def team_season_stats():
     # Be sure to include any advanced stats
     return "This is a test flask api setup!  :)"
 
+@app.route('/api/nba/team/seasonAverages', methods=['POST'])
+def team_season_average_stats():
+    if request.is_json:
+        data = request.get_json()
+        team_name = data.get('teamName', '')
+        season = request.get_json()['season']
+        return TeamStats.get_team_season_stats(team_name, season)
+    # Be sure to include any advanced stats
+    return "This is a test flask api setup!  :)"
+
 
 @app.route('/api/nba/team/playoffStats', methods=['POST'])
 def team_playoff_stats():
@@ -61,5 +80,13 @@ def team_playoff_stats():
         season = request.get_json()['season']
         return TeamStats.get_team_playoff_stats(team_name, season)
 
+
+@app.route('/api/nba/team/playoffStatsAverage', methods=['POST'])
+def team_playoff_average_stats():
+    if request.is_json:
+        data = request.get_json()
+        team_name = data.get('teamName', '')
+        season = request.get_json()['season']
+        return TeamStats.get_team_playoff_stats(team_name, season)
 if __name__ == '__main__':
     app.run()
