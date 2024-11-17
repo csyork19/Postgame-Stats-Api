59ff774879be5f4f152cd8d2b814cb87d97446ed
Storm York <85768942+csyork19@users.noreply.github.com>
2024-11-17 01:00:49 -0500

    Add NBA Shot chart

---
+++ Postgame Stats/ShotChartUtil.py
@@ -0,0 +1,442 @@
+# Data and NBA API
+import pandas as pd
+import numpy as np
+
+from scipy.stats import percentileofscore
+
+pd.options.display.max_columns = None
+from nba_api.stats.static import players
+from nba_api.stats.endpoints import shotchartdetail
+from nba_api.stats.endpoints import playercareerstats
+
+# For Shot Chart
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+from matplotlib.patches import Circle, Rectangle, Arc
+from matplotlib.collections import PatchCollection
+from matplotlib.colors import ListedColormap, BoundaryNorm
+from matplotlib.path import Path
+from matplotlib.patches import PathPatch
+
+sns.set_style('white')
+sns.set_color_codes()
+
+
+def get_player_shotchartdetail(player_name, season_id):
+    """
+    Parameters
+    ----------
+    player_name: name of the player with Capital
+    season_id: ex. 2012-13
+    """
+
+    # player dictionary
+    nba_players = players.get_players()
+    player_dict = [player for player in nba_players if player['full_name'] == player_name][0]
+
+    # career df
+    career = playercareerstats.PlayerCareerStats(player_id=player_dict['id'])
+    career_df = career.get_data_frames()[0]
+
+    # team id during the season
+    team_id = career_df[career_df['SEASON_ID'] == season_id]['TEAM_ID']
+
+    # shotchardtdetail endpoint
+    shotchartlist = shotchartdetail.ShotChartDetail(team_id=team_id,player_id=player_dict['id'],
+                                                    season_type_all_star='Regular Season',season_nullable=season_id,
+                                                    context_measure_simple="FGA").get_data_frames()
+
+    return shotchartlist[0], shotchartlist[1]
+
+
+def draw_court(ax=None, color="blue", lw=1, shotzone=False, outer_lines=False):
+    """Returns an axes with a basketball court drawn onto to it.
+    This function draws a court based on the x and y-axis values that the NBA
+    stats API provides for the shot chart data.  For example the center of the
+    hoop is located at the (0,0) coordinate.  Twenty-two feet from the left of
+    the center of the hoop in is represented by the (-220,0) coordinates.
+    So one foot equals +/-10 units on the x and y-axis.
+    Parameters
+    ----------
+    ax : Axes, optional
+        The Axes object to plot the court onto.
+    color : matplotlib color, optional
+        The color of the court lines.
+    lw : float, optional
+        The linewidth the of the court lines.
+    outer_lines : boolean, optional
+        If `True` it draws the out of bound lines in same style as the rest of
+        the court.
+    Returns
+    -------
+    ax : Axes
+        The Axes object with the court on it.
+    """
+    if ax is None:
+        ax = plt.gca()
+
+    # Create the various parts of an NBA basketball court
+
+    # Create the basketball hoop
+    hoop = Circle((0, 0), radius=7.5, linewidth=lw, color=color, fill=False)
+
+    # Create backboard
+    backboard = Rectangle((-30, -12.5), 60, 0, linewidth=lw, color=color)
+
+    # The paint
+    # Create the outer box 0f the paint, width=16ft, height=19ft
+    outer_box = Rectangle((-80, -47.5), 160, 190, linewidth=lw, color=color,
+                          fill=False)
+    # Create the inner box of the paint, widt=12ft, height=19ft
+    inner_box = Rectangle((-60, -47.5), 120, 190, linewidth=lw, color=color,
+                          fill=False)
+
+    # Create free throw top arc
+    top_free_throw = Arc((0, 142.5), 120, 120, theta1=0, theta2=180,
+                         linewidth=lw, color=color, fill=False)
+    # Create free throw bottom arc
+    bottom_free_throw = Arc((0, 142.5), 120, 120, theta1=180, theta2=0,
+                            linewidth=lw, color=color, linestyle='dashed')
+    # Restricted Zone, it is an arc with 4ft radius from center of the hoop
+    restricted = Arc((0, 0), 80, 80, theta1=0, theta2=180, linewidth=lw,
+                     color=color)
+
+    # Three point line
+    # Create the right side 3pt lines, it's 14ft long before it arcs
+    corner_three_a = Rectangle((-220, -47.5), 0, 140, linewidth=lw,
+                               color=color)
+    # Create the right side 3pt lines, it's 14ft long before it arcs
+    corner_three_b = Rectangle((220, -47.5), 0, 140, linewidth=lw, color=color)
+    # 3pt arc - center of arc will be the hoop, arc is 23'9" away from hoop
+    three_arc = Arc((0, 0), 475, 475, theta1=22, theta2=158, linewidth=lw,
+                    color=color)
+
+    # Center Court
+    center_outer_arc = Arc((0, 422.5), 120, 120, theta1=180, theta2=0,
+                           linewidth=lw, color=color)
+    center_inner_arc = Arc((0, 422.5), 40, 40, theta1=180, theta2=0,
+                           linewidth=lw, color=color)
+
+    # Draw shotzone Lines
+    # Based on Advanced Zone Mode
+    if (shotzone == True):
+        inner_circle = Circle((0, 0), radius=80, linewidth=lw, color='black', fill=False)
+        outer_circle = Circle((0, 0), radius=160, linewidth=lw, color='black', fill=False)
+        corner_three_a_x = Rectangle((-250, 92.5), 30, 0, linewidth=lw, color=color)
+        corner_three_b_x = Rectangle((220, 92.5), 30, 0, linewidth=lw, color=color)
+
+        # 60 degrees
+        inner_line_1 = Rectangle((40, 69.28), 80, 0, linewidth=lw, color=color)
+        # 120 degrees
+        inner_line_2 = Rectangle((-40, 69.28), 80, 0, linewidth=lw, color=color)
+
+        # Assume x distance is also 40 for the endpoint
+        inner_line_3 = Rectangle((53.20, 150.89), 290, 0, linewidth=lw, color=color)
+        inner_line_4 = Rectangle((-53.20, 150.89), 290, 0, linewidth=lw, color=color)
+
+        # Assume y distance is also 92.5 for the endpoint
+        inner_line_5 = Rectangle((130.54, 92.5), 80, 0, linewidth=lw, color=color)
+        inner_line_6 = Rectangle((-130.54, 92.5), 80, 0, linewidth=lw, color=color)
+
+        # List of the court elements to be plotted onto the axes
+        court_elements = [hoop, backboard, outer_box, inner_box, top_free_throw,
+                          bottom_free_throw, restricted, corner_three_a,
+                          corner_three_b, three_arc, center_outer_arc,
+                          center_inner_arc, inner_circle, outer_circle,
+                          corner_three_a_x, corner_three_b_x,
+                          inner_line_1, inner_line_2, inner_line_3, inner_line_4, inner_line_5, inner_line_6]
+    else:
+        # List of the court elements to be plotted onto the axes
+        court_elements = [hoop, backboard, outer_box, inner_box, top_free_throw,
+                          bottom_free_throw, restricted, corner_three_a,
+                          corner_three_b, three_arc, center_outer_arc,
+                          center_inner_arc]
+
+    if outer_lines:
+        # Draw the half court line, baseline and side out bound lines
+        outer_lines = Rectangle((-250, -47.5), 500, 470, linewidth=lw,
+                                color=color, fill=False)
+        court_elements.append(outer_lines)
+
+    # Add the court elements onto the axes
+    for element in court_elements:
+        ax.add_patch(element)
+
+    return ax
+
+
+def shot_chart(data, title="", color="b",
+               xlim=(-250, 250), ylim=(422.5, -47.5), line_color="blue",
+               court_color="white", court_lw=2, outer_lines=False,
+               flip_court=False, gridsize=None,
+               ax=None, despine=False, **kwargs):
+    if ax is None:
+        ax = plt.gca()
+
+    if not flip_court:
+        ax.set_xlim(xlim)
+        ax.set_ylim(ylim)
+    else:
+        ax.set_xlim(xlim[::-1])
+        ax.set_ylim(ylim[::-1])
+
+    ax.tick_params(labelbottom="off", labelleft="off")
+    ax.set_title(title, fontsize=18)
+
+    # draws the court
+    draw_court(ax, color=line_color, lw=court_lw, outer_lines=outer_lines)
+
+    # separate color by make or miss
+    x_missed = data[data['EVENT_TYPE'] == 'Missed Shot']['LOC_X']
+    y_missed = data[data['EVENT_TYPE'] == 'Missed Shot']['LOC_Y']
+
+    x_made = data[data['EVENT_TYPE'] == 'Made Shot']['LOC_X']
+    y_made = data[data['EVENT_TYPE'] == 'Made Shot']['LOC_Y']
+
+    # plot missed shots
+    ax.scatter(x_missed, y_missed, c='r', marker="x", s=300, linewidths=3, **kwargs)
+    # plot made shots
+    ax.scatter(x_made, y_made, facecolors='none', edgecolors='g', marker="o", s=100, linewidths=3, **kwargs)
+
+    # Set the spines to match the rest of court lines, makes outer_lines
+    # somewhate unnecessary
+    for spine in ax.spines:
+        ax.spines[spine].set_lw(court_lw)
+        ax.spines[spine].set_color(line_color)
+
+    if despine:
+        ax.spines["top"].set_visible(False)
+        ax.spines["bottom"].set_visible(False)
+        ax.spines["right"].set_visible(False)
+        ax.spines["left"].set_visible(False)
+
+    return ax
+
+
+def sized_hexbin(ax, hc, hc2, cmap, norm):
+    offsets = hc.get_offsets()
+    orgpath = hc.get_paths()[0]
+    verts = orgpath.vertices
+    values1 = hc.get_array()
+    values2 = hc2.get_array()
+    ma = values1.max()
+    patches = []
+
+    for offset, val in zip(offsets, values1):
+        # Adding condition for minimum size
+        # offset is the respective position of each hexagons
+
+        # remove 0 to compare frequency without 0s
+        filtered_list = list(filter(lambda num: num != 0, values1))
+
+        # we also skip frequency counts that are 0s
+        # this is to discount hexbins with no occurences
+        # default value hexagons are the frequencies
+        if (int(val) == 0):
+            continue
+        elif (percentileofscore(filtered_list, val) < 33.33):
+            # print(percentileofscore(values1, val))
+            # print("bot")
+            v1 = verts * 0.3 + offset
+        elif (percentileofscore(filtered_list, val) > 69.99):
+            # print(percentileofscore(values1, val))
+            # print("top")
+            v1 = verts + offset
+        else:
+            # print("mid")
+            v1 = verts * 0.6 + offset
+
+        path = Path(v1, orgpath.codes)
+        patch = PathPatch(path)
+        patches.append(patch)
+
+    pc = PatchCollection(patches, cmap=cmap, norm=norm)
+    # sets color
+    # so hexbin with C=data['FGP']
+    pc.set_array(values2)
+
+    ax.add_collection(pc)
+    hc.remove()
+    hc2.remove()
+
+
+def hexmap_chart(data, league_avg, title="", color="b",
+                 xlim=(-250, 250), ylim=(422.5, -47.5), line_color="white",
+                 court_color="#1a477b", court_lw=2, outer_lines=False,
+                 flip_court=False, gridsize=None,
+                 ax=None, despine=False, **kwargs):
+    LA = league_avg.loc[:, ['SHOT_ZONE_AREA', 'SHOT_ZONE_RANGE', 'FGA', 'FGM']].groupby(
+        ['SHOT_ZONE_AREA', 'SHOT_ZONE_RANGE']).sum()
+    LA['FGP'] = 1.0 * LA['FGM'] / LA['FGA']
+    player = data.groupby(['SHOT_ZONE_AREA', 'SHOT_ZONE_RANGE', 'SHOT_MADE_FLAG']).size().unstack(fill_value=0)
+    player['FGP'] = 1.0 * player.loc[:, 1] / player.sum(axis=1)
+    player_vs_league = (player.loc[:, 'FGP'] - LA.loc[:, 'FGP']) * 100
+
+    data = pd.merge(data, player_vs_league, on=['SHOT_ZONE_AREA', 'SHOT_ZONE_RANGE'], how='right')
+
+    if ax is None:
+        ax = plt.gca()
+        ax.set_facecolor(court_color)
+
+    if not flip_court:
+        ax.set_xlim(xlim)
+        ax.set_ylim(ylim)
+    else:
+        ax.set_xlim(xlim[::-1])
+        ax.set_ylim(ylim[::-1])
+
+    ax.tick_params(labelbottom="off", labelleft="off")
+    ax.set_title(title, fontsize=18)
+
+    # draws the court
+    draw_court(ax, color=line_color, lw=court_lw, outer_lines=outer_lines)
+
+    x = data['LOC_X']
+    y = data['LOC_Y']
+
+    # for diverging color map
+    colors = ['#2b7cb6', '#abd9e9', '#ffffbf', '#fdaf61', '#d7191c']
+    cmap = ListedColormap(colors)
+    # The 5 colors are separated by -9, -3, 0, 3, 9
+    boundaries = [-np.inf, -9, -3, 0, 3, 9, np.inf]
+    norm = BoundaryNorm(boundaries, len(boundaries) - 1, clip=True)
+
+
+
+    # first hexbin required for bincount
+    # second hexbin for the coloring of each hexagons
+    hexbin = ax.hexbin(x, y, gridsize=40, cmap=cmap, norm=norm, extent=[-275, 275, -50, 425])
+    hexbin2 = ax.hexbin(x, y, C=data['FGP'], gridsize=40, cmap=cmap, norm=norm, extent=[-275, 275, -50, 425])
+    sized_hexbin(ax, hexbin, hexbin2, cmap, norm)
+
+    # Set the spines to match the rest of court lines, makes outer_lines
+    # somewhate unnecessary
+    for spine in ax.spines:
+        ax.spines[spine].set_lw(court_lw)
+        ax.spines[spine].set_color(line_color)
+
+    if despine:
+        ax.spines["top"].set_visible(False)
+        ax.spines["bottom"].set_visible(False)
+        ax.spines["right"].set_visible(False)
+        ax.spines["left"].set_visible(False)
+
+    return ax
+
+
+def shot_zones(data, league_avg, title="", color="b",
+               xlim=(-250, 250), ylim=(422.5, -47.5), line_color="black",
+               court_color="white", court_lw=2, outer_lines=False,
+               flip_court=False, ax=None, despine=False, **kwargs):
+    if ax is None:
+        ax = plt.gca()
+        ax.set_facecolor(court_color)
+
+    if not flip_court:
+        ax.set_xlim(xlim)
+        ax.set_ylim(ylim)
+    else:
+        ax.set_xlim(xlim[::-1])
+        ax.set_ylim(ylim[::-1])
+
+    ax.tick_params(labelbottom="off", labelleft="off")
+    ax.set_title(title, fontsize=18)
+
+    # draws the court
+    # set shotzone to True
+    draw_court(ax, color=line_color, lw=court_lw, shotzone=True, outer_lines=outer_lines)
+
+    LA = league_avg.loc[:, ['SHOT_ZONE_AREA', 'SHOT_ZONE_RANGE', 'FGA', 'FGM']].groupby(
+        ['SHOT_ZONE_AREA', 'SHOT_ZONE_RANGE']).sum()
+    LA['FGP'] = 1.0 * LA['FGM'] / LA['FGA']
+    print(LA)
+
+    player = data.groupby(['SHOT_ZONE_AREA', 'SHOT_ZONE_RANGE', 'SHOT_MADE_FLAG']).size().unstack(fill_value=0)
+    player['FGP'] = 1.0 * player.loc[:, 1] / player.sum(axis=1)
+    player_vs_league = (player.loc[:, 'FGP'] - LA.loc[:, 'FGP']) * 100
+    print(player_vs_league)
+
+    data = pd.merge(data, player_vs_league, on=['SHOT_ZONE_AREA', 'SHOT_ZONE_RANGE'], how='right')
+
+    x_made = data[data['EVENT_TYPE'] == 'Made Shot']['LOC_X']
+    y_made = data[data['EVENT_TYPE'] == 'Made Shot']['LOC_Y']
+
+    # plot missed shots
+    # ax.scatter(x_missed, y_missed, c='r', marker="x", s=300, linewidths=3, **kwargs)
+    # plot made shots
+    ax.scatter(x_made, y_made, facecolors='none', edgecolors='b', s=100, linewidths=3, **kwargs)
+
+    # Set the spines to match the rest of court lines, makes outer_lines
+    # somewhate unnecessary
+    for spine in ax.spines:
+        ax.spines[spine].set_lw(court_lw)
+        ax.spines[spine].set_color(line_color)
+
+    if despine:
+        ax.spines["top"].set_visible(False)
+        ax.spines["bottom"].set_visible(False)
+        ax.spines["right"].set_visible(False)
+        ax.spines["left"].set_visible(False)
+
+
+def heatmap(data, title="", color="b",
+            xlim=(-250, 250), ylim=(422.5, -47.5), line_color="white",
+            court_color="white", court_lw=2, outer_lines=False,
+            flip_court=False, gridsize=None,
+            ax=None, despine=False, **kwargs):
+    if ax is None:
+        ax = plt.gca()
+
+    if not flip_court:
+        ax.set_xlim(xlim)
+        ax.set_ylim(ylim)
+    else:
+        ax.set_xlim(xlim[::-1])
+        ax.set_ylim(ylim[::-1])
+
+    ax.tick_params(labelbottom="off", labelleft="off")
+    ax.set_title(title, fontsize=18)
+
+    # draws the court
+    draw_court(ax, color=line_color, lw=court_lw, outer_lines=outer_lines)
+
+    x = data['LOC_X']
+    y = data['LOC_Y']
+
+    sns.kdeplot(x, y, shade=True, cmap='inferno', ax=ax, **kwargs)
+
+    ax.scatter(x, y, facecolors='w', s=2, linewidths=0.1, **kwargs)
+
+    # Set the spines to match the rest of court lines, makes outer_lines
+    # somewhate unnecessary
+    for spine in ax.spines:
+        ax.spines[spine].set_lw(court_lw)
+        ax.spines[spine].set_color(line_color)
+
+    if despine:
+        ax.spines["top"].set_visible(False)
+        ax.spines["bottom"].set_visible(False)
+        ax.spines["right"].set_visible(False)
+        ax.spines["left"].set_visible(False)
+
+    return ax
+
+
+
+
+
+
+player_name = 'Klay Thompson'
+year = '2023-24'
+
+player_shotchart_df, league_avg = get_player_shotchartdetail(player_name, year)
+# Set the size for our plots
+plt.rcParams['figure.figsize'] = (12, 11)
+shot_chart(player_shotchart_df, title=str(player_name))
+plt.show()
+hexmap_chart(player_shotchart_df, league_avg, title=str(player_name) + " Hex Chart " + str(year))
+plt.show()
+shot_zones(player_shotchart_df, league_avg, title=str(player_name) + " Heat Map " + str(year))
+# heatmap(player_shotchart_df)
+# plt.show()
