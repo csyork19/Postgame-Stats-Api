3ac0b5bd3043c03c3381f6e2b0ef7bcf707e1959
csyork19 <csyork19@gmail.com>
2024-11-25 10:13:41 -0500

    Cleanup methods - part 1

--- Postgame Stats/NbaPlayerStats.py
+++ Postgame Stats/NbaPlayerStats.py
@@ -10,67 +10,41 @@ def get_player_id(self):
     return jsonify(int(player_id))
 
 def get_player_stats(self):
-    if self:
-        desired_columns = [
-            "AST", "BLK", "DREB", "FG3A", "FG3M", "FG3_PCT", "FGA", "FGM", "FG_PCT",
-            "FTA", "FTM", "FT_PCT", "GAME_DATE", "MATCHUP", "MIN", "OREB", "PF",
-            "PLUS_MINUS", "PTS", "REB", "STL", "TOV", "WL"
-        ]
+    # Fetch the player game logs
+    nba_player_logs = playergamelog.PlayerGameLog(player_id=PostGameStatsUtil.PostGameStatsUtil.get_player_id(self))
+    nba_player_logs_df = nba_player_logs.get_data_frames()[0]
 
-        # Fetch the player game logs
-        player_logs = playergamelog.PlayerGameLog(player_id=PostGameStatsUtil.PostGameStatsUtil.get_player_id(self))
-        player_logs_df = player_logs.get_data_frames()[0]
+    nba_player_stat_columns = [
+         "AST", "BLK", "DREB", "FG3A", "FG3M", "FG3_PCT", "FGA", "FGM", "FG_PCT",
+        "FTA", "FTM", "FT_PCT", "GAME_DATE", "MATCHUP", "MIN", "OREB", "PF",
+        "PLUS_MINUS", "PTS", "REB", "STL", "TOV", "WL"
+    ]
 
-        # Filter the DataFrame to only include the desired columns
-        filtered_player_logs_df = player_logs_df[desired_columns]
-
-        # Return the filtered DataFrame as a dictionary
-        return filtered_player_logs_df.to_dict()
-    return None
+    return nba_player_logs_df[nba_player_stat_columns].to_dict()
 
 def get_player_season_average(self):
-    player_logs = playergamelog.PlayerGameLog(player_id=PostGameStatsUtil.PostGameStatsUtil.get_player_id(self))
-    player_logs_df = player_logs.get_data_frames()[0]
+    nba_player_logs = playergamelog.PlayerGameLog(player_id=PostGameStatsUtil.PostGameStatsUtil.get_player_id(self))
+    nba_player_logs_df = nba_player_logs.get_data_frames()[0]
 
     # Define the columns you want to average
-    columns_to_average = [
+    nba_player_stat_columns = [
         "AST", "BLK", "DREB", "FG3A", "FG3M", "FG3_PCT", "FGA", "FGM", "FG_PCT",
         "FTA", "FTM", "FT_PCT", "MIN", "OREB", "PF", "PLUS_MINUS", "PTS", "REB", "STL", "TOV"
     ]
 
     # Filter the DataFrame to only include these columns
-    filtered_df = player_logs_df[columns_to_average]
-    averages = filtered_df.mean()
-    averages = averages.round(3)
-    return averages.to_dict()
-
+    filtered_df = nba_player_logs_df[nba_player_stat_columns].mean().round(2).to_dict()
+    return filtered_df
 
 def get_player_career_stats(self):
-    if self:
-        # player dictionary
-        nba_players = players.get_players()
-        player_dict = [player for player in nba_players if player['full_name'] == self][0]
-
-        # career df
-        career = playercareerstats.PlayerCareerStats(player_id=player_dict['id'])
-        career_df = career.get_data_frames()[0]
-        return career_df.to_dict();
-        # player_career = playercareerstats.PlayerCareerStats(
-        #     player_id=PostGameStatsUtil.PostGameStatsUtil.get_player_id(self))
-        # player_career_df = player_career.get_data_frames()[0]
-        # return player_career_df.to_dict()
-
+    nba_players = players.get_players()
+    nba_player_dict = [player for player in nba_players if player['full_name'] == self][0]
+    return playercareerstats.PlayerCareerStats(player_id=nba_player_dict['id']).get_data_frames()[0].to_dict()
 
 def get_player_playoff_stats(self, year):
-    if self:
-        player_logs = playergamelog.PlayerGameLog(
-            player_id=PostGameStatsUtil.PostGameStatsUtil.get_player_id(self),
-            season=year,
-            season_type_all_star='Playoffs')
-        player_logs_df = player_logs.get_data_frames()[0]
-        return player_logs_df.to_dict()
-
+    return playergamelog.PlayerGameLog(player_id=PostGameStatsUtil.PostGameStatsUtil.get_player_id(self),
+                                       season=year,
+                                       season_type_all_star='Playoffs').get_data_frames()[0].to_dict()
 
 def get_player_shot_chart_coordinates(self, year):
-    if self:
-        return PostGameStatsUtil.PostGameStatsUtil.get_player_season_shot_chart(self, year)
+    return PostGameStatsUtil.PostGameStatsUtil.get_player_season_shot_chart(self, year)
