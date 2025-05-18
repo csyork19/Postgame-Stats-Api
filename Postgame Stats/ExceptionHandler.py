732818c8c49e1ad4ef1f06a489e6486e381862f5
Storm York <85768942+csyork19@users.noreply.github.com>
2025-05-18 08:40:59 -0400

    Code cleanup

--- Postgame Stats/ExceptionHandler.py
+++
@@ -1,6 +0,0 @@
-class PostGameStatsException(Exception):
-    def __init__(self, message):
-        self.message = message
-
-    def __str__(self):
-        return f"Post Game Stats API: {self.message}".to_dict()
--- Postgame Stats/dao/PostGameStatsServiceDao.py
+++
@@ -1,6 +0,0 @@
-from common_util import PostGameStatsUtil
-
-class PostGameStatDao:
-    def get_player_id(self):
-        player_id = PostGameStatsUtil.PostGameStatsUtil.get_player_id(self)
-        return jsonify(int(player_id))
