7b767e1e51d60d7b7dc7dbb4cfd6c85b92f4e509
Storm York <85768942+csyork19@users.noreply.github.com>
2025-10-01 19:09:44 -0400

    Clean up NFL route

--- Postgame Stats/routes/nfl.py
+++ Postgame Stats/routes/nfl.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, jsonify, request
+from flask import Blueprint, request
 
 import NFLStats
 from decorators.errors import handle_exceptions
@@ -12,10 +12,10 @@ nfl_player_bp = Blueprint("nfl_player_bp", __name__, url_prefix="/api/nfl/player
 @jwt_required
 @handle_exceptions
 @with_json(required_fields=["playerName", "season", "seasonType"])
-def get_nfl_player_season_stats() -> 'flask.Response':
-    player_name = request.get_json()['playerName']
-    season = request.get_json()['season']
-    seasonType = request.get_json()['seasonType']
+def get_nfl_player_season_stats(request) -> 'flask.Response':
+    player_name = request['playerName']
+    season = request['season']
+    seasonType = request['seasonType']
     response = NFLStats.NFLPlayerStats.get_nfl_player_stats(player_name, season, seasonType)
     return response
 
@@ -24,10 +24,10 @@ def get_nfl_player_season_stats() -> 'flask.Response':
 @jwt_required
 @handle_exceptions
 @with_json(required_fields=["playerName", "season", "seasonType"])
-def get_nfl_player_rushing_season_stats() -> 'flask.Response':
-    player_name = request.get_json()['playerName']
-    season = request.get_json()['season']
-    seasonType = request.get_json()['seasonType']
+def get_nfl_player_rushing_season_stats(request) -> 'flask.Response':
+    player_name = request['playerName']
+    season = request['season']
+    seasonType = request['seasonType']
     response = NFLStats.NFLPlayerStats.get_nfl_player_rushing_stats(player_name, season, seasonType)
     return response
 
@@ -36,10 +36,10 @@ def get_nfl_player_rushing_season_stats() -> 'flask.Response':
 @jwt_required
 @handle_exceptions
 @with_json(required_fields=["playerName", "season", "seasonType"])
-def get_nfl_player_receiving_season_stats() -> 'flask.Response':
-    player_name = request.get_json()['playerName']
-    season = request.get_json()['season']
-    seasonType = request.get_json()['seasonType']
+def get_nfl_player_receiving_season_stats(request) -> 'flask.Response':
+    player_name = request['playerName']
+    season = request['season']
+    seasonType = request['seasonType']
     response = NFLStats.NFLPlayerStats.get_nfl_player_receiving_stats(player_name, season, seasonType)
     return response
 
@@ -48,8 +48,8 @@ def get_nfl_player_receiving_season_stats() -> 'flask.Response':
 @jwt_required
 @handle_exceptions
 @with_json(required_fields=["teamName", "season"])
-def get_nfl_pbp_team_season_stats() -> 'flask.Response':
-    team_name = request.get_json()['teamName']
-    season = request.get_json()['season']
+def get_nfl_pbp_team_season_stats(request) -> 'flask.Response':
+    team_name = request['teamName']
+    season = request['season']
     response = NFLStats.NFLTeamStats.get_nfl_pbp_team_stats(team_name, season)
     return response
